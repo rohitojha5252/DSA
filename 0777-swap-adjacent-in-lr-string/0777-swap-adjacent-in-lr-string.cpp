40fc97ebc9d3f5084a9272f577aae8e45aa7345a
rohitojha5252 <rohitojha5252@gmail.com>
2026-07-06 14:48:02 +0530

    Time: 0 ms (100%), Space: 9.8 MB (27.37%) - LeetHub

---
+++ 0777-swap-adjacent-in-lr-string/0777-swap-adjacent-in-lr-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canTransform(string start, string result) {
+        int n = start.size();
+        int i = 0, j = 0;
+
+        while(i < n || j < n){
+            while (i < n && start[i] == 'X' )i++;
+            while(j < n && result[j] == 'X' )j++;
+
+            if (i == n || j == n)
+                return i == n && j == n;
+
+            if(start[i] != result[j])return false;
+            if(start[i] == 'L' && i < j)return false;
+            if(start[i] == 'R' && j < i) return false;
+
+            i++;
+            j++;
+        }
+        return i == n && j == n;
+    }
+};
