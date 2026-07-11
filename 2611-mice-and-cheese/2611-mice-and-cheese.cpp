e389b534e3337749aa0572290fe66f20b31da92a
rohitojha5252 <rohitojha5252@gmail.com>
2026-07-11 14:51:21 +0530

    Time: 57 ms (20.4%), Space: 114.7 MB (25.87%) - LeetHub

---
+++ 2611-mice-and-cheese/2611-mice-and-cheese.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int miceAndCheese(vector<int>& reward1, vector<int>& reward2, int k) {
+        int n = reward1.size();
+        priority_queue<pair<int, int>> pq;
+
+        for(int i=0; i<n; i++){
+            pq.push({reward1[i] - reward2[i], i});
+        }
+        int ans = 0;
+        while(k--){
+            int ind = pq.top().second;
+            pq.pop();
+            ans += reward1[ind];
+            reward2[ind] = 0;
+        }
+        for(int i=0; i<n; i++){
+            ans+=reward2[i];
+        }
+        return ans;
+    }
+};
