f7265ff77797a294b849fb3592d7b142db0c4472
rohitojha5252 <rohitojha5252@gmail.com>
2026-07-12 13:02:01 +0530

    Time: 215 ms (8.54%), Space: 205.8 MB (5.98%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,24 +1,34 @@
 class Solution {
 public:
-    int dp[201][20001];
-    int func(int ind, int sum, vector<int>& nums){
-        if(sum == 0)return true;
-        if(ind<0)return false;
+    vector<vector<int>>dp;
+    bool func(vector<int>&arr, int sum , int i){
+        int n = arr.size();
+        if(i==n){
+            if(sum == 0){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
         
-        if(dp[ind][sum] != -1)return dp[ind][sum];
-
-        bool ispossible = func(ind-1, sum, nums);
-
-        if(sum - nums[ind] >=0)ispossible |= func(ind-1,sum-nums[ind], nums);
-
-        return dp[ind][sum] = ispossible;
+        if(dp[i][sum] != -1) return dp[i][sum];
+        
+        if(arr[i]>sum){
+            return func(arr, sum, i+1);
+        }
+        
+        int c1 = func(arr, sum-arr[i],i+1);
+        int c2 = func(arr, sum, i+1);
+        
+        return dp[i][sum] = c1 || c2;
     }
     bool canPartition(vector<int>& nums) {
+        int n = nums.size();
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        if(sum%2 != 0 )return false;
-
-        memset(dp, -1, sizeof(dp));
-
-        return func(nums.size() - 1, sum/2, nums);
+        if(sum%2 == 1)return false;
+        dp.assign(n+1, vector<int>(sum+1, -1));
+        
+        return func(nums, sum/2, 0);
     }
 };
