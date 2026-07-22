c99ca48c102da0845148ade0961a1528e5ce3359
rohitojha5252 <rohitojha5252@gmail.com>
2026-07-22 10:06:16 +0530

    Time: 34 ms (8.96%), Space: 34.8 MB (8.41%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<int>>dp;
+    bool func(string s1, string s2, string s3, int i, int j){
+        int n = s1.size();
+        int m = s2.size();
+        if(i+j == s3.size() && i==n && j == m){
+            return true;
+        }
+        if(dp[i][j] != -1)return dp[i][j];
+        bool takei = false;
+        bool takej = false;
+        
+        if(i<n && s1[i] == s3[i+j])takei = func(s1, s2, s3, i+1, j);
+        if(j<m && s2[j] == s3[i+j])takej = func(s1, s2, s3, i, j+1);
+       
+        return dp[i][j] = takei || takej;
+
+    }
+    bool isInterleave(string s1, string s2, string s3) {
+        dp.assign(s1.size()+1, vector<int>(s2.size()+1, -1));
+        return func(s1, s2, s3, 0, 0);
+    }
+};
